35b5fd9837a1629c70de89ef58237d45c6a0482c
Zack Newman <znewman01@gmail.com>
2013-03-12 14:19:37 -0400

    Add check for taken endpoint
    When trying to install a cron job to point to a particular endpoint, if
    it is taken, give an error message instead of the regular install
    script.

--- routes/install.js
+++ routes/install.js
@@ -2,10 +2,24 @@
  * GET install script
  */
 
+var db = require('../db');
+
+var mongo = db.mongo;
+var mongo_db = db.mongo_db;
 
 exports.install = function(host, port) {
   return function(req, res){
     var name = req.params[0];
-    res.render('install', { host: host, port: port, name: name });
+    mongo.open(function(err, client) {
+      var db = client.db(mongo_db);
+      db.collection('coll').count({ 'name': name }, function(err, found){
+        if (!found) {
+          res.render('install', { host: host, port: port, name: name });
+        } else {
+          res.render('taken', { name: name });
+        }
+        client.close();
+      });
+    });
   };
 };
